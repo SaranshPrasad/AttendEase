23a70d83d2e565217c655f0245bd571bd50075fa
SaranshPrasad <saranshprasad08@gmail.com>
2025-11-12 09:18:05 +0530

    issue fixed

--- backend/src/routes/facultyRouter.js
+++ backend/src/routes/facultyRouter.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const MasterFaculty = require("../models/master_faculty");
 const cookieParser = require("cookie-parser");
-const { default: Course } = require("../../../frontend/src/lib/Course");
 const Timetable = require("../models/timetable");
 const facultyRouter = express.Router();
 
